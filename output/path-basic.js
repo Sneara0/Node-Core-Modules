6513255d3e9b0c8c518e61c342852688e2fbfae4
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-23 00:06:20 +0600

    add path-basic

---
+++ output/path-basic.js
@@ -0,0 +1,16 @@
+const { console } = require('inspector');
+const path= require('path');
+console.log('current file info:\n')
+console.log('file name',__filename);
+console.log('dir name', __dirname);
+
+console.log("\n" + "-".repeat(20) + "\n")
+const filePath='/sneara/document/nextLevel.pdf';
+console.log('analyzing path:',filePath,'\n');
+console.log('Directory:',path.dirname(filePath));
+console.log('Base name:',path.basename(filePath));
+
+console.log('file extension:',path.extname(filePath));
+console.log('file name:',path.basename(filePath,path.extname(filePath)))
+
+
