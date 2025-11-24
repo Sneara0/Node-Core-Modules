452eb2d4b4ee7e04e05241c9a2346dbad9164f02
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-24 22:05:07 +0600

    add os basics.js

--- output/path-basic.js
+++ output/path-basic.js
@@ -1,4 +1,4 @@
-const { console } = require('inspector');
+
 const path= require('path');
 console.log('current file info:\n')
 console.log('file name',__filename);
@@ -9,7 +9,7 @@ const filePath='/sneara/document/nextLevel.pdf';
 console.log('analyzing path:',filePath,'\n');
 console.log('Directory:',path.dirname(filePath));
 console.log('Base name:',path.basename(filePath));
-
+console.log("\n" + "-".repeat(20) + "\n")
 console.log('file extension:',path.extname(filePath));
 console.log('file name:',path.basename(filePath,path.extname(filePath)))
 
